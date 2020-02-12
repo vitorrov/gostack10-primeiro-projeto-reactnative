b98e739491638a00ee30fc35eab8385b0c077014
Vitor <vitormr45@gmail.com>
2020-02-12 18:04:28 -0300

    Minimal updates

---
+++ ios/build/Project/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/react-native-webview.build/Objects-normal/x86_64/RNCWKProcessPoolManager.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/vitor/Desktop/GoStack/gostack10-primeiro-projeto-reactnative/node_modules/react-native-webview/ios/RNCWKProcessPoolManager.m \
+  /Users/vitor/Desktop/GoStack/gostack10-primeiro-projeto-reactnative/ios/Pods/Target\ Support\ Files/react-native-webview/react-native-webview-prefix.pch \
+  /Users/vitor/Desktop/GoStack/gostack10-primeiro-projeto-reactnative/node_modules/react-native-webview/ios/RNCWKProcessPoolManager.h
---
+++ ios/build/Project/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/react-native-webview.build/Objects-normal/x86_64/react-native-webview-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/vitor/Desktop/GoStack/gostack10-primeiro-projeto-reactnative/ios/Pods/Target\ Support\ Files/react-native-webview/react-native-webview-dummy.m \
+  /Users/vitor/Desktop/GoStack/gostack10-primeiro-projeto-reactnative/ios/Pods/Target\ Support\ Files/react-native-webview/react-native-webview-prefix.pch
